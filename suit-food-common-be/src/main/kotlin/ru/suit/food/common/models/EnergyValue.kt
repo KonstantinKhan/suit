459e07e234f81607beb72ee2c4fd890719d9c5f7
KonstantinKhan <khan366kos@gmail.com>
2024-08-17 12:15:34 +0500

    updated EnergyValue

--- suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/EnergyValue.kt
+++ suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/EnergyValue.kt
@@ -1,5 +1,8 @@
 package ru.suit.food.common.models
 
 data class EnergyValue(
-    val calories: Measure = Measure()
+    val calories: Measure = Measure(),
+    val proteins: Measure = Measure(),
+    val fats: Measure = Measure(),
+    val carbohydrates: Measure = Measure()
 )
