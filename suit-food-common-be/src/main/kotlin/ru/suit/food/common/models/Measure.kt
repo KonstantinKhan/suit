ea737d0bf10a72153a1209a647ff70d8131e6742
KonstantinKhan <khan366kos@gmail.com>
2024-08-17 12:26:24 +0500

    updated Measure

--- suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/Measure.kt
+++ suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/Measure.kt
@@ -1,7 +1,6 @@
 package ru.suit.food.common.models
 
 data class Measure(
-    val measureName: String = "",
     val measureValue: Double = 0.0,
     val unit: UnitMeasure = UnitMeasure()
 )
--- suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/UnitMeasure.kt
+++ suit-food-common-be/src/main/kotlin/ru/suit/food/common/models/UnitMeasure.kt
@@ -2,5 +2,6 @@ package ru.suit.food.common.models
 
 data class UnitMeasure(
     val unitMeasureId: UnitMeasureId = UnitMeasureId.NONE,
+    val unitMeasureName: String = "",
     val unitMeasureValue: String = ""
 )
