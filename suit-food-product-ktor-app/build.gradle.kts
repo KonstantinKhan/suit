7ee4cf3a269a316fc1070e57bbbc85300960ddae
KonstantinKhan <khan366kos@gmail.com>
2024-08-25 20:21:57 +0500

    added ktor module

---
+++ suit-food-product-ktor-app/build.gradle.kts
@@ -0,0 +1,24 @@
+val ktorVersion: String by project
+val logbackVersion: String by project
+
+plugins {
+    kotlin("jvm")
+    id("io.ktor.plugin") version "2.3.12"
+}
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
+}
